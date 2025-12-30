b8b9fd61d99a5c9680c7457fed389c99d3dd2d97
Nishar <mohamednisharj@gmail.com>
2025-12-30 18:40:14 +0530

    Initial Commit

---
+++ TraficIntelligence/src/test/java/com/example/TraficIntelligence/TraficIntelligenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TraficIntelligence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraficIntelligenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
